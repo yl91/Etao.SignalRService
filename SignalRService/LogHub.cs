ec496fea7b55b092ebf189bc9ea67b45f7bdc075
yangli_sky@163.com <yangli_sky@163.com>
2016-06-24 17:28:05 +0800

    a

---
+++ SignalRService/LogHub.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalRService
+{
+    [HubName("pushHub")]
+    public class LogHub:Hub
+    {
+        public override Task OnConnected()
+        {
+            var username = this.Context.QueryString["username"];
+            var groupName= this.Context.QueryString["groupname"];
+            JoinGroup(groupName);
+            Clients.Group(groupName).addMessage(1,$"有人重连了,username:{username}");
+            return base.OnConnected();
+        }
+
+        public override Task OnReconnected()
+        {
+            var username = this.Context.QueryString["username"];
+            string groupName = this.Context.QueryString["groupname"];
+            JoinGroup(groupName);
+            Clients.Group(groupName).addMessage(2,$"有人重连了,username:{username}");
+            return base.OnReconnected();
+        }
+
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            var username = this.Context.QueryString["username"];
+            string groupName = this.Context.QueryString["groupname"];
+            LeaveGroup(groupName);
+            Clients.Group(groupName).addMessage(3,$"有人断开连接了,username:{username}");
+            return base.OnDisconnected(stopCalled);
+        }
+        public Task JoinGroup(string groupName)
+        {
+            return Groups.Add(Context.ConnectionId, groupName);
+        }
+
+        public Task LeaveGroup(string groupName)
+        {
+            return Groups.Remove(Context.ConnectionId, groupName);
+        }
+
+    }
+}
